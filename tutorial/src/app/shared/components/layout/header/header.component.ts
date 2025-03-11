3d876657d538da5cccfa542ec08919c71396b498
Angela Torro <a.torroballester@gmail.com>
2025-03-11 13:42:40 +0100

    add logout functionality and update header component for user session management

--- tutorial/src/app/shared/components/layout/header/header.component.ts
+++ tutorial/src/app/shared/components/layout/header/header.component.ts
@@ -1,18 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
   username: string | null = null;
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.checkLoginStatus();
@@ -26,5 +38,12 @@ export class HeaderComponent implements OnInit {
     } else {
       this.username = null;
     }
+  }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('username');
+    this.username = null;
+    this.router.navigate(['/login']);
   }
 }
